208ccbca5699e9bfdb31dd3788ff15126045187e
Joshua Clayton <jclayton@thoughtbot.com>
2015-07-16 12:13:33 -0400

    Refactor feature

--- spec/features/user_creates_todo_spec.rb
+++ spec/features/user_creates_todo_spec.rb
@@ -3,10 +3,15 @@ require "rails_helper"
 feature "User creates todo" do
   scenario "successfully" do
     visit root_path
-    click_on "Create todo"
-    fill_in "Title", with: "Buy eggs"
-    click_on "Submit"
+
+    create_todo_titled title: "Buy eggs"
 
     expect(page).to have_css "ul.todos", text: "Buy eggs"
+  end
+
+  def create_todo_titled(title:)
+    click_on "Create todo"
+    fill_in "Title", with: title
+    click_on "Submit"
   end
 end
